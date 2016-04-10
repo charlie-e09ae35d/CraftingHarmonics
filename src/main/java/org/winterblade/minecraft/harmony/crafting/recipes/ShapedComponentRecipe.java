4503780f79ffcd451b76e2b65c4d567b28128e24
Matt <legendblade@users.noreply.github.com>
2016-04-09 17:10:44 -0700

    Putting in the rest of the skeleton for the ShapedComponentRecipe class.  Now for something completely different.

--- src/main/java/org/winterblade/minecraft/harmony/crafting/recipes/ShapedComponentRecipe.java
+++ src/main/java/org/winterblade/minecraft/harmony/crafting/recipes/ShapedComponentRecipe.java
@@ -4,12 +4,17 @@ import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.oredict.OreDictionary;
 import org.winterblade.minecraft.harmony.crafting.components.RecipeComponent;
 
 /**
  * Created by Matt on 4/9/2016.
  */
 public class ShapedComponentRecipe implements IRecipe {
+    public static final int MAX_CRAFT_GRID_WIDTH = 3;
+    public static final int MAX_CRAFT_GRID_HEIGHT = 3;
+
     private final int width;
     private final int height;
     private final RecipeComponent[] input;
@@ -24,26 +29,68 @@ public class ShapedComponentRecipe implements IRecipe {
 
     @Override
     public boolean matches(InventoryCrafting inv, World worldIn) {
+        for (int x = 0; x <= MAX_CRAFT_GRID_WIDTH - width; x++)
+        {
+            for (int y = 0; y <= MAX_CRAFT_GRID_HEIGHT - height; ++y)
+            {
+                if (checkMatch(inv, x, y, worldIn))
+                {
+                    return true;
+                }
+            }
+        }
+
         return false;
+    }
+
+    protected boolean checkMatch(InventoryCrafting inv, int startX, int startY, World world)
+    {
+        for (int x = 0; x < MAX_CRAFT_GRID_WIDTH; x++)
+        {
+            for (int y = 0; y < MAX_CRAFT_GRID_HEIGHT; y++)
+            {
+                int subX = x - startX;
+                int subY = y - startY;
+                RecipeComponent target = null;
+
+                if (subX >= 0 && subY >= 0 && subX < width && subY < height)
+                {
+                    target = input[width - subX - 1 + subY * width];
+                }
+
+                ItemStack slot = inv.getStackInRowAndColumn(x, y);
+
+                if(target == null) {
+                    if(slot != null) return false;
+                    continue;
+                }
+
+                // TODO: Run matchers here...
+
+            }
+        }
+
+        return true;
     }
 
     @Override
     public ItemStack getCraftingResult(InventoryCrafting inv) {
-        return null;
+        return output.getItemStack().copy();
     }
 
     @Override
     public int getRecipeSize() {
-        return 0;
+        return input.length;
     }
 
     @Override
     public ItemStack getRecipeOutput() {
-        return null;
+        return output.getItemStack();
     }
 
     @Override
     public ItemStack[] getRemainingItems(InventoryCrafting inv) {
-        return new ItemStack[0];
+        // TODO: Not this.
+        return ForgeHooks.defaultRecipeGetRemainingItems(inv);
     }
 }
