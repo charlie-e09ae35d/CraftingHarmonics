2a2c564d36f71ab632375626c8f2acca88ebf240
Matt <legendblade@users.noreply.github.com>
2016-04-12 17:41:59 -0700

    Bumping version and updating changelog.

--- src/main/java/org/winterblade/minecraft/harmony/CraftingHarmonicsMod.java
+++ src/main/java/org/winterblade/minecraft/harmony/CraftingHarmonicsMod.java
@@ -30,7 +30,7 @@ import static net.minecraftforge.oredict.RecipeSorter.Category.SHAPELESS;
 @Mod(modid = org.winterblade.minecraft.harmony.CraftingHarmonicsMod.MODID, version = org.winterblade.minecraft.harmony.CraftingHarmonicsMod.VERSION)
 public class CraftingHarmonicsMod {
     public static final String MODID = "craftingharmonics";
-    public static final String VERSION = "1.9.0-1.1.3";
+    public static final String VERSION = "1.9.0-1.2.0";
 
     private String configPath;
     private ConfigManager configManager;
