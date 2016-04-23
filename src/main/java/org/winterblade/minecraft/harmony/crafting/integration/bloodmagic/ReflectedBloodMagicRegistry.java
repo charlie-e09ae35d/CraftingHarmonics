cad46a3a2189e954ac1832de033e44fef8245ea8
Matt <legendblade@users.noreply.github.com>
2016-04-22 18:23:24 -0700

    Dealing with alchemy arrays; which are a pain.

--- src/main/java/org/winterblade/minecraft/harmony/crafting/integration/bloodmagic/ReflectedBloodMagicRegistry.java
+++ src/main/java/org/winterblade/minecraft/harmony/crafting/integration/bloodmagic/ReflectedBloodMagicRegistry.java
@@ -1,15 +1,22 @@
 package org.winterblade.minecraft.harmony.crafting.integration.bloodmagic;
 
 import WayofTime.bloodmagic.api.ItemStackWrapper;
+import WayofTime.bloodmagic.api.alchemyCrafting.AlchemyArrayEffect;
+import WayofTime.bloodmagic.api.alchemyCrafting.AlchemyCircleRenderer;
 import WayofTime.bloodmagic.api.recipe.TartaricForgeRecipe;
 import WayofTime.bloodmagic.api.registry.AlchemyArrayRecipeRegistry;
+import WayofTime.bloodmagic.api.registry.AlchemyArrayRecipeRegistry.AlchemyArrayRecipe;
 import WayofTime.bloodmagic.api.registry.AltarRecipeRegistry;
 import WayofTime.bloodmagic.api.registry.TartaricForgeRecipeRegistry;
 import com.google.common.collect.BiMap;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.oredict.OreDictionary;
 
-import java.util.List;
+import javax.annotation.Nullable;
+import java.util.*;
+import java.util.Map.Entry;
 
 /**
  * Created by Matt on 4/21/2016.
@@ -17,7 +24,7 @@ import java.util.List;
 public class ReflectedBloodMagicRegistry {
     private static BiMap<List<ItemStack>, AltarRecipeRegistry.AltarRecipe> altarRecipes;
     private static List<TartaricForgeRecipe> tartaricForgeRecipes;
-    private static BiMap<List<ItemStack>, AlchemyArrayRecipeRegistry.AlchemyArrayRecipe> alchemyArrayRecipes;
+    private static BiMap<List<ItemStack>, AlchemyArrayRecipe> alchemyArrayRecipes;
 
     static {
         // I really wish mod authors would better support removing recipes from their mods...
@@ -42,5 +49,110 @@ public class ReflectedBloodMagicRegistry {
 
     public static void removeHellfireForgeRecipe(TartaricForgeRecipe recipe) {
         tartaricForgeRecipes.remove(recipe);
+    }
+
+    public static void addAlchemyArray(ItemStack input, @Nullable ItemStack catalystStack, AlchemyArrayEffect arrayEffect, ResourceLocation circleRenderer) {
+        AlchemyArrayRecipeRegistry.registerRecipe(Collections.singletonList(input), catalystStack, arrayEffect, circleRenderer);
+    }
+
+    public static void addAlchemyArray(String input, @Nullable ItemStack catalystStack, AlchemyArrayEffect arrayEffect, ResourceLocation circleRenderer) {
+        AlchemyArrayRecipeRegistry.registerRecipe(input, catalystStack, arrayEffect, circleRenderer);
+    }
+
+    public static void addAlchemyArray(List<ItemStack> input, @Nullable ItemStack catalyst, AlchemyArrayEffect effect, AlchemyCircleRenderer renderer) {
+        AlchemyArrayRecipeRegistry.registerRecipe(input, catalyst, effect, renderer);
+    }
+
+    /**
+     * Remove an alchemy array with the given item stack / catalyst combo
+     * @param inputItem    The ItemStack to check against
+     * @param catalyst     The catalyst to check against; can be null
+     * @return             The removed recipe; null if none matched.
+     */
+    public static RemovedAlchemyArray removeAlchemyArray(ItemStack inputItem, @Nullable ItemStack catalyst) {
+        return removeAlchemyArray(inputItem == null ? null : Collections.singletonList(inputItem), catalyst);
+    }
+
+    /**
+     * Remove an alchemy array with the given ore dictionary / catalyst combo
+     * @param input        The ore dictionary to check against
+     * @param catalyst     The catalyst to check against; can be null
+     * @return             The removed recipe; null if none matched.
+     */
+    public static RemovedAlchemyArray removeAlchemyArray(String input, ItemStack catalyst) {
+        return removeAlchemyArray(OreDictionary.getOres(input), catalyst);
+    }
+
+    /**
+     * Remove an alchemy array with the given ItemStack list / catalyst combo
+     * @param input        The ItemStack list to check against
+     * @param catalyst     The catalyst to check against; can be null
+     * @return             The removed recipe; null if none matched.
+     */
+    public static RemovedAlchemyArray removeAlchemyArray(List<ItemStack> input, @Nullable ItemStack catalyst) {
+        for (Iterator<Entry<List<ItemStack>, AlchemyArrayRecipe>> it = alchemyArrayRecipes.entrySet().iterator(); it.hasNext(); )
+        {
+            AlchemyArrayRecipe recipe = it.next().getValue();
+
+            // Check if we match...
+            if(!alchemyArrayMatches(recipe, input, catalyst)) continue;
+
+            // Find our effect:
+            AlchemyArrayEffect effect = null;
+            ItemStackWrapper wrapper = null;
+            for(Iterator<Entry<ItemStackWrapper, AlchemyArrayEffect>> catIt = recipe.catalystMap.entrySet().iterator(); catIt.hasNext(); ) {
+                Entry<ItemStackWrapper, AlchemyArrayEffect> catalystEntry = catIt.next();
+                if(!catalystEntry.getKey().toStack().isItemEqualIgnoreDurability(catalyst)) continue;
+
+                // Set our effect, then remove the catalyst:
+                effect = catalystEntry.getValue();
+                catIt.remove();
+                break;
+            }
+
+            // Remove it if we only had the one catalyst
+            if(recipe.catalystMap.size() <= 0) {
+                it.remove();
+            }
+
+            // Finally, return our removed recipe
+            return new RemovedAlchemyArray(input, catalyst, effect, recipe.circleRenderer);
+        }
+
+        return null;
+    }
+
+    /**
+     * Checks the given recipe against the input/catalyst
+     * @param recipe    The recipe to check
+     * @param input     The input to check against
+     * @param catalyst  The catalyst to check against
+     * @return          True if the recipe matches; false otherwise
+     */
+    private static boolean alchemyArrayMatches(AlchemyArrayRecipe recipe, List<ItemStack> input, @Nullable ItemStack catalyst) {
+        return recipe.doesInputMatchRecipe(input)&& recipe.getAlchemyArrayEffectForCatalyst(catalyst) != null;
+    }
+
+    /**
+     * Adds a recipe directly to the alchemy array
+     * @param recipe    The recipe to add
+     */
+    public static void addAlchemyArray(AlchemyArrayRecipe recipe) {
+        alchemyArrayRecipes.put(recipe.getInput(), recipe);
+    }
+
+    public static class RemovedAlchemyArray {
+        public List<ItemStack> input;
+        public ItemStack catalyst;
+        public AlchemyArrayEffect effect;
+        public AlchemyCircleRenderer renderer;
+
+        public RemovedAlchemyArray(List<ItemStack> input, ItemStack catalyst,
+                                   AlchemyArrayEffect effect, AlchemyCircleRenderer renderer) {
+            this.input = input;
+            this.catalyst = catalyst;
+            this.effect = effect;
+            this.renderer = renderer;
+        }
     }
 }
