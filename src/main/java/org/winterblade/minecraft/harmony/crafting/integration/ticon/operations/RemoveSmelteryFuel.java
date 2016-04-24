83739217905aeb43d3c693e6362547c577c66049
Matt <legendblade@users.noreply.github.com>
2016-04-24 12:44:23 -0700

    Adding ability to remove fuels from the smeltery.

---
+++ src/main/java/org/winterblade/minecraft/harmony/crafting/integration/ticon/operations/RemoveSmelteryFuel.java
@@ -0,0 +1,43 @@
+package org.winterblade.minecraft.harmony.crafting.integration.ticon.operations;
+
+import net.minecraftforge.fluids.FluidStack;
+import org.winterblade.minecraft.harmony.CraftingHarmonicsMod;
+import org.winterblade.minecraft.harmony.api.BaseRecipeOperation;
+import org.winterblade.minecraft.harmony.api.RecipeOperation;
+import org.winterblade.minecraft.harmony.crafting.ItemMissingException;
+import org.winterblade.minecraft.harmony.crafting.integration.ticon.ReflectedTinkerRegistry;
+
+import java.util.Map;
+
+/**
+ * Created by Matt on 4/24/2016.
+ */
+@RecipeOperation(name = "removeSmelteryFuel", dependsOn = "tconstruct")
+public class RemoveSmelteryFuel extends BaseRecipeOperation {
+    /*
+     * Serialized properties
+     */
+    private FluidStack what;
+
+    /*
+     * Computed properties
+     */
+    private transient Map.Entry<FluidStack, Integer> removedFuel;
+
+    @Override
+    public void Init() throws ItemMissingException {
+
+    }
+
+    @Override
+    public void Apply() {
+        CraftingHarmonicsMod.logger.info("Removing Tinker's smeltery fuel using '" + what.getFluid().getName() + "'.");
+        removedFuel = ReflectedTinkerRegistry.removeFuel(what);
+    }
+
+    @Override
+    public void Undo() {
+        if(removedFuel == null) return;
+        ReflectedTinkerRegistry.addFuel(removedFuel.getKey(), removedFuel.getValue());
+    }
+}
