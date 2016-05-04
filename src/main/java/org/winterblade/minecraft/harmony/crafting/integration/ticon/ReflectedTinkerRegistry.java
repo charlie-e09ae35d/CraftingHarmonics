5d2bb043ff41b0f3cdb48967fda86371711cf4e1
Matt <legendblade@users.noreply.github.com>
2016-05-04 16:19:15 -0700

    Adding ability to set harvest levels in TiCon.

--- src/main/java/org/winterblade/minecraft/harmony/crafting/integration/ticon/ReflectedTinkerRegistry.java
+++ src/main/java/org/winterblade/minecraft/harmony/crafting/integration/ticon/ReflectedTinkerRegistry.java
@@ -1,14 +1,15 @@
 package org.winterblade.minecraft.harmony.crafting.integration.ticon;
 
-import com.google.common.collect.Maps;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.library.client.CustomFontColor;
 import slimeknights.tconstruct.library.materials.IMaterialStats;
 import slimeknights.tconstruct.library.materials.Material;
 import slimeknights.tconstruct.library.smeltery.AlloyRecipe;
 import slimeknights.tconstruct.library.smeltery.CastingRecipe;
 import slimeknights.tconstruct.library.traits.ITrait;
+import slimeknights.tconstruct.library.utils.HarvestLevels;
 
 import java.util.Iterator;
 import java.util.List;
@@ -18,15 +19,22 @@ import java.util.Map;
  * Created by Matt on 4/21/2016.
  */
 public class ReflectedTinkerRegistry {
+    // Registry
     private static List<AlloyRecipe> alloyRegistry;
     private static Map<FluidStack, Integer> smelteryFuels;
     private static Map<String, Material> materials;
+
+    // Harvest levels:
+    public static Map<Integer, String> harvestLevelNames;
 
     static {
         // Hook directly into this because we can't remove an alloy after it's been added:
         alloyRegistry = ObfuscationReflectionHelper.getPrivateValue(TinkerRegistry.class, null, "alloyRegistry");
         smelteryFuels = ObfuscationReflectionHelper.getPrivateValue(TinkerRegistry.class, null, "smelteryFuels");
         materials = ObfuscationReflectionHelper.getPrivateValue(TinkerRegistry.class, null, "materials");
+
+        // Harvest levels:
+        harvestLevelNames = ObfuscationReflectionHelper.getPrivateValue(HarvestLevels.class, null, "harvestLevelNames");
     }
 
     private ReflectedTinkerRegistry() {}
@@ -138,5 +146,39 @@ public class ReflectedTinkerRegistry {
      */
     public static void setMaterial(Material material) {
         materials.put(material.getIdentifier(), material);
+    }
+
+    /**
+     * Gets the harvest level at the given value:
+     * @param level    The level to get
+     * @return         The harvest level (with color)
+     */
+    public static String getHarvestLevelName(int level) {
+        return harvestLevelNames.get(level);
+    }
+
+    /**
+     * Sets a harvest level to the given name:
+     * @param level    The level to set
+     * @param name     The name to set it to
+     */
+    public static void setHarvestLevelName(int level, String name) {
+
+        harvestLevelNames.put(level, name);
+    }
+
+    /**
+     * Encodes a color
+     * @param color    The color to encode
+     * @return         The string representation of the color
+     */
+    public static String encodeColor(String color) {
+        if(color == null || color.equals("") || color.length() != 7) return CustomFontColor.encodeColor(0xFFFFFF);
+
+        Integer r = Integer.valueOf(color.substring(1, 3), 16);
+        Integer g = Integer.valueOf(color.substring(3, 5), 16);
+        Integer b = Integer.valueOf(color.substring(5, 7), 16);
+
+        return CustomFontColor.encodeColor(r, g, b);
     }
 }
---
+++ src/main/java/org/winterblade/minecraft/harmony/crafting/integration/ticon/operations/SetHarvestLevelName.java
@@ -0,0 +1,42 @@
+package org.winterblade.minecraft.harmony.crafting.integration.ticon.operations;
+
+import org.winterblade.minecraft.harmony.api.BaseRecipeOperation;
+import org.winterblade.minecraft.harmony.api.RecipeOperation;
+import org.winterblade.minecraft.harmony.crafting.ItemMissingException;
+import org.winterblade.minecraft.harmony.crafting.integration.ticon.ReflectedTinkerRegistry;
+
+/**
+ * Created by Matt on 5/4/2016.
+ */
+@RecipeOperation(name = "setHarvestLevelName", dependsOn = "tconstruct")
+public class SetHarvestLevelName extends BaseRecipeOperation {
+    /*
+     * Serialized properties
+     */
+    private int level;
+    private String name;
+    private String color;
+
+    /*
+     * Computed properties
+     */
+    private transient String newLevel;
+    private transient String oldLevel;
+
+    @Override
+    public void Init() throws ItemMissingException {
+        if(level < 0) throw new ItemMissingException("Harvest level to set cannot be less than zero.");
+        oldLevel = ReflectedTinkerRegistry.getHarvestLevelName(level);
+        newLevel = ReflectedTinkerRegistry.encodeColor(color) + name;
+    }
+
+    @Override
+    public void Apply() {
+        ReflectedTinkerRegistry.setHarvestLevelName(level, newLevel);
+    }
+
+    @Override
+    public void Undo() {
+        ReflectedTinkerRegistry.setHarvestLevelName(level, oldLevel);
+    }
+}
