54f2ba37606c3c192d849e4d23ed1d3b2a2e526c
Matt <legendblade@users.noreply.github.com>
2016-05-03 19:56:03 -0700

    Adding a way to replace TiCon materials.

--- src/main/java/org/winterblade/minecraft/harmony/crafting/integration/ticon/ReflectedTinkerRegistry.java
+++ src/main/java/org/winterblade/minecraft/harmony/crafting/integration/ticon/ReflectedTinkerRegistry.java
@@ -1,10 +1,14 @@
 package org.winterblade.minecraft.harmony.crafting.integration.ticon;
 
+import com.google.common.collect.Maps;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.library.materials.IMaterialStats;
+import slimeknights.tconstruct.library.materials.Material;
 import slimeknights.tconstruct.library.smeltery.AlloyRecipe;
 import slimeknights.tconstruct.library.smeltery.CastingRecipe;
+import slimeknights.tconstruct.library.traits.ITrait;
 
 import java.util.Iterator;
 import java.util.List;
@@ -16,11 +20,13 @@ import java.util.Map;
 public class ReflectedTinkerRegistry {
     private static List<AlloyRecipe> alloyRegistry;
     private static Map<FluidStack, Integer> smelteryFuels;
+    private static Map<String, Material> materials;
 
     static {
         // Hook directly into this because we can't remove an alloy after it's been added:
         alloyRegistry = ObfuscationReflectionHelper.getPrivateValue(TinkerRegistry.class, null, "alloyRegistry");
         smelteryFuels = ObfuscationReflectionHelper.getPrivateValue(TinkerRegistry.class, null, "smelteryFuels");
+        materials = ObfuscationReflectionHelper.getPrivateValue(TinkerRegistry.class, null, "materials");
     }
 
     private ReflectedTinkerRegistry() {}
@@ -85,5 +91,52 @@ public class ReflectedTinkerRegistry {
      */
     public static void removeBasinCast(CastingRecipe recipe) {
         TinkerRegistry.getAllBasinCastingRecipes().remove(recipe);
+    }
+
+    /**
+     * Gets the given material from the registry, or null if it's unknown
+     * @param identifier    The identifier to get
+     * @return              The material, or null if it doesn't exist
+     */
+    public static Material getMaterial(String identifier) {
+        Material material = TinkerRegistry.getMaterial(identifier);
+        if(material == Material.UNKNOWN) return null;
+
+        return material;
+    }
+
+    /**
+     * Creates a copy of the given material
+     * @param material      The material to copy
+     * @return              The copied material
+     */
+    public static Material copyMaterial(Material material) {
+        Material output = new Material(material.identifier, material.materialTextColor);
+        output.setFluid(material.getFluid());
+        output.setCraftable(material.isCraftable());
+        output.setCastable(material.isCastable());
+        output.setRenderInfo(material.renderInfo);
+
+        material.getDefaultTraits().forEach(output::addTrait);
+
+        for(IMaterialStats stats : material.getAllStats()) {
+            output.addStats(stats);
+            for(ITrait trait : output.getAllTraitsForStats(stats.getIdentifier())) {
+                output.addTrait(trait, stats.getIdentifier());
+            }
+        }
+
+        output.setRepresentativeItem(material.getRepresentativeItem());
+        output.setShard(material.getShard());
+
+        return output;
+    }
+
+    /**
+     * Set a material on the materials list
+     * @param material    The material to set
+     */
+    public static void setMaterial(Material material) {
+        materials.put(material.getIdentifier(), material);
     }
 }
