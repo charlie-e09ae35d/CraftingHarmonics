95c5d6f5b9c9586f451a51c6b06fb911f1cf5de1
Matt <legendblade@users.noreply.github.com>
2016-05-08 13:47:58 -0700

    Compressor.

---
+++ src/main/java/org/winterblade/minecraft/harmony/crafting/integration/techreborn/operations/AddCompressorOperation.java
@@ -0,0 +1,19 @@
+package org.winterblade.minecraft.harmony.crafting.integration.techreborn.operations;
+
+import org.winterblade.minecraft.harmony.api.RecipeOperation;
+import org.winterblade.minecraft.harmony.crafting.ItemMissingException;
+import org.winterblade.minecraft.harmony.crafting.integration.techreborn.RebornRecipeUtils;
+import reborncore.api.recipe.IBaseRecipeType;
+import techreborn.api.recipe.machines.CompressorRecipe;
+
+/**
+ * Created by Matt on 5/8/2016.
+ */
+@RecipeOperation(name = "TechReborn.addCompressor", dependsOn = RebornRecipeUtils.TechRebornModId)
+public class AddCompressorOperation extends BaseTechRebornAddOperation {
+    public AddCompressorOperation() {super("Compressor");}
+    @Override
+    protected IBaseRecipeType getRecipe() throws ItemMissingException {
+        return new CompressorRecipe(getInput(0), getOutput(0), ticks, euPerTick);
+    }
+}
