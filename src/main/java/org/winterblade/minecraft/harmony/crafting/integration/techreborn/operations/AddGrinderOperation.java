b4f0e17655ec81cbc988c88f6108f45c4b3e4198
Matt <legendblade@users.noreply.github.com>
2016-05-08 14:00:18 -0700

    Actually adding the file contents this time.  Whoops.

--- src/main/java/org/winterblade/minecraft/harmony/crafting/integration/techreborn/operations/AddGrinderOperation.java
+++ src/main/java/org/winterblade/minecraft/harmony/crafting/integration/techreborn/operations/AddGrinderOperation.java
@@ -1,7 +1,20 @@
 package org.winterblade.minecraft.harmony.crafting.integration.techreborn.operations;
 
+import org.winterblade.minecraft.harmony.api.RecipeOperation;
+import org.winterblade.minecraft.harmony.crafting.ItemMissingException;
+import org.winterblade.minecraft.harmony.crafting.integration.techreborn.RebornRecipeUtils;
+import reborncore.api.recipe.IBaseRecipeType;
+import techreborn.api.recipe.machines.GrinderRecipe;
+
 /**
  * Created by Matt on 5/8/2016.
  */
-public class AddGrinderOperation {
+@RecipeOperation(name = "TechReborn.addGrinder", dependsOn = RebornRecipeUtils.TechRebornModId)
+public class AddGrinderOperation extends BaseTechRebornAddOperation {
+    public AddGrinderOperation() {super("Grinder");}
+
+    @Override
+    protected IBaseRecipeType getRecipe() throws ItemMissingException {
+        return new GrinderRecipe(getInput(0), getOutput(0), ticks, euPerTick);
+    }
 }
