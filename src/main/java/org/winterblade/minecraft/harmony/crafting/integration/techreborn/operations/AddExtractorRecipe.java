5af33eee553f64f38f616c2c1edeebe710fe2620
Matt <legendblade@users.noreply.github.com>
2016-05-08 13:53:43 -0700

    Extractor.

---
+++ src/main/java/org/winterblade/minecraft/harmony/crafting/integration/techreborn/operations/AddExtractorRecipe.java
@@ -0,0 +1,20 @@
+package org.winterblade.minecraft.harmony.crafting.integration.techreborn.operations;
+
+import org.winterblade.minecraft.harmony.api.RecipeOperation;
+import org.winterblade.minecraft.harmony.crafting.ItemMissingException;
+import org.winterblade.minecraft.harmony.crafting.integration.techreborn.RebornRecipeUtils;
+import reborncore.api.recipe.IBaseRecipeType;
+import techreborn.api.recipe.machines.ExtractorRecipe;
+
+/**
+ * Created by Matt on 5/8/2016.
+ */
+@RecipeOperation(name = "TechReborn.addExtractor", dependsOn = RebornRecipeUtils.TechRebornModId)
+public class AddExtractorRecipe extends BaseTechRebornAddOperation {
+    public AddExtractorRecipe() {super("Extractor");}
+
+    @Override
+    protected IBaseRecipeType getRecipe() throws ItemMissingException {
+        return new ExtractorRecipe(getInput(0), getOutput(0), ticks, euPerTick);
+    }
+}
