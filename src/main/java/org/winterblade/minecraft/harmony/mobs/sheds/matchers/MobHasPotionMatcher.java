7793baa4af6f7872129db858073172bc681c1916
Matt <legendblade@users.noreply.github.com>
2016-05-12 16:12:46 -0700

    Copying over the relevant matchers from MobDrops to MobSheds.

---
+++ src/main/java/org/winterblade/minecraft/harmony/mobs/sheds/matchers/MobHasPotionMatcher.java
@@ -0,0 +1,34 @@
+package org.winterblade.minecraft.harmony.mobs.sheds.matchers;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import org.winterblade.minecraft.harmony.api.Component;
+import org.winterblade.minecraft.harmony.api.PrioritizedObject;
+import org.winterblade.minecraft.harmony.api.Priority;
+import org.winterblade.minecraft.harmony.api.drops.BaseDropMatchResult;
+import org.winterblade.minecraft.harmony.api.mobs.sheds.IMobShedMatcher;
+import org.winterblade.minecraft.harmony.drops.matchers.BaseHasPotionMatcher;
+
+/**
+ * Created by Matt on 5/12/2016.
+ */
+@Component(properties = {"mobHasPotionEffect"})
+@PrioritizedObject(priority = Priority.MEDIUM)
+public class MobHasPotionMatcher extends BaseHasPotionMatcher implements IMobShedMatcher {
+    public MobHasPotionMatcher(Potion potion) {
+        super(potion);
+    }
+
+    /**
+     * Should return true if this matcher matches the given event
+     *
+     * @param entity    The event to match
+     * @param drop      The drop that will be generated if true
+     * @return          True if it should match; false otherwise
+     */
+    @Override
+    public BaseDropMatchResult isMatch(EntityLiving entity, ItemStack drop) {
+        return matches(entity);
+    }
+}
---
+++ src/main/java/org/winterblade/minecraft/harmony/mobs/sheds/matchers/NotMatcher.java
@@ -0,0 +1,39 @@
+package org.winterblade.minecraft.harmony.mobs.sheds.matchers;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.item.ItemStack;
+import org.winterblade.minecraft.harmony.api.Component;
+import org.winterblade.minecraft.harmony.api.PrioritizedObject;
+import org.winterblade.minecraft.harmony.api.Priority;
+import org.winterblade.minecraft.harmony.api.drops.BaseDropMatchResult;
+import org.winterblade.minecraft.harmony.api.mobs.sheds.IMobShedMatcher;
+import org.winterblade.minecraft.harmony.mobs.sheds.MobShed;
+
+/**
+ * Created by Matt on 5/12/2016.
+ */
+@Component(properties = {"not"})
+@PrioritizedObject(priority = Priority.MEDIUM)
+public class NotMatcher implements IMobShedMatcher {
+    private final MobShed composite;
+
+    public NotMatcher(MobShed composite) {
+        this.composite = composite;
+    }
+
+    /**
+     * Should return true if this matcher matches the given event
+     *
+     * @param entity           The event to match
+     * @param drop             The dropped item; this can be modified.
+     * @return True if it should match; false otherwise
+     */
+    @Override
+    public BaseDropMatchResult isMatch(EntityLiving entity, ItemStack drop) {
+        // We aren't going to run any of the callbacks from here, as they're inverted.
+        BaseDropMatchResult result = composite.matches(entity, drop);
+
+        // Just invert the match:
+        return new BaseDropMatchResult(!result.isMatch());
+    }
+}
