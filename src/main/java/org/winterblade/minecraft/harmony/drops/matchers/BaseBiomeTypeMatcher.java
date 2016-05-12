0211a8d5a589d96e8615f63d92720826f4d48abb
Matt <legendblade@users.noreply.github.com>
2016-05-11 21:36:32 -0700

    Biome tag matchers for mob drops.

---
+++ src/main/java/org/winterblade/minecraft/harmony/drops/matchers/BaseBiomeTypeMatcher.java
@@ -0,0 +1,43 @@
+package org.winterblade.minecraft.harmony.drops.matchers;
+
+import net.minecraft.entity.Entity;
+import net.minecraftforge.common.BiomeDictionary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Matt on 5/11/2016.
+ */
+public class BaseBiomeTypeMatcher {
+    private final List<BiomeDictionary.Type> types = new ArrayList<>();
+
+    public BaseBiomeTypeMatcher(String[] biomesTags) {
+        // Add all our tags:
+        for(String tag : biomesTags) {
+            tag = tag.toUpperCase();
+            for(BiomeDictionary.Type type : BiomeDictionary.Type.values()) {
+                if(!type.name().equals(tag)) continue;
+                types.add(type);
+                break;
+            }
+        }
+    }
+
+    protected boolean matches(Entity entity) {
+        if(entity == null) return false;
+
+        // Get all the tags for this biome:
+        BiomeDictionary.Type[] biomeTags = BiomeDictionary.getTypesForBiome(
+                entity.getEntityWorld().getBiomeGenForCoords(entity.getPosition()));
+
+        // Figure out if we match:
+        for(BiomeDictionary.Type type : biomeTags) {
+            if(!types.contains(type)) continue;
+            return true;
+        }
+
+        return false;
+    }
+
+}
