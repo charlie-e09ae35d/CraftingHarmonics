fb5ede595a37c490fb108e108969e005cf0d722c
Matt <legendblade@users.noreply.github.com>
2016-05-12 18:20:12 -0700

    Adding composite matchers.

---
+++ src/main/java/org/winterblade/minecraft/harmony/mobs/drops/matchers/AndMatcher.java
@@ -0,0 +1,65 @@
+package org.winterblade.minecraft.harmony.mobs.drops.matchers;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import org.winterblade.minecraft.harmony.api.Component;
+import org.winterblade.minecraft.harmony.api.PrioritizedObject;
+import org.winterblade.minecraft.harmony.api.Priority;
+import org.winterblade.minecraft.harmony.api.drops.BaseDropMatchResult;
+import org.winterblade.minecraft.harmony.api.mobs.drops.IMobDropMatcher;
+import org.winterblade.minecraft.harmony.mobs.drops.MobDrop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Matt on 5/12/2016.
+ */
+@Component(properties = {"and"})
+@PrioritizedObject(priority = Priority.MEDIUM)
+public class AndMatcher implements IMobDropMatcher {
+    private final MobDrop[] composites;
+
+    public AndMatcher(MobDrop[] composites) {
+        this.composites = composites;
+    }
+
+    /**
+     * Should return true if this matcher matches the given event
+     *
+     * @param livingDropsEvent The event to match
+     * @param drop             The dropped item; this can be modified.
+     * @return True if it should match; false otherwise
+     */
+    @Override
+    public BaseDropMatchResult isMatch(LivingDropsEvent livingDropsEvent, ItemStack drop) {
+        if(composites == null || composites.length <= 0) return BaseDropMatchResult.False;
+
+        List<Runnable> runnables = new ArrayList<>();
+        int callbacks = 0;
+
+        for(MobDrop composite : composites) {
+            BaseDropMatchResult result = composite.matches(livingDropsEvent, drop);
+
+            // If we didn't match, we failed:
+            if(!result.isMatch()) return BaseDropMatchResult.False;
+
+            // Add any callbacks we need to do:
+            if(result.getCallback() != null) {
+                runnables.add(result.getCallback());
+                callbacks++;
+            }
+        }
+
+        // If we can handle this easily, do so:
+        if(callbacks <= 0) return BaseDropMatchResult.True;
+        if(callbacks == 1) return new BaseDropMatchResult(true, runnables.get(0));
+
+        // Return a composite function:
+        return new BaseDropMatchResult(true, () -> {
+            for(Runnable runnable : runnables) {
+                runnable.run();
+            }
+        });
+    }
+}
