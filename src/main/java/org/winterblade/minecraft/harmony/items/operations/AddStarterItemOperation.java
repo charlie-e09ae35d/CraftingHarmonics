052e1ef307bc53e23c0a7195c022acbc3cc7984f
Matt <legendblade@users.noreply.github.com>
2016-05-17 19:22:24 -0700

    Adding logging.

--- src/main/java/org/winterblade/minecraft/harmony/items/operations/AddStarterItemOperation.java
+++ src/main/java/org/winterblade/minecraft/harmony/items/operations/AddStarterItemOperation.java
@@ -1,11 +1,13 @@
 package org.winterblade.minecraft.harmony.items.operations;
 
+import com.google.common.base.Joiner;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import org.winterblade.minecraft.harmony.api.BasePerPlayerOperation;
 import org.winterblade.minecraft.harmony.api.RecipeOperation;
 import org.winterblade.minecraft.harmony.crafting.ItemMissingException;
 import org.winterblade.minecraft.harmony.crafting.ItemRegistry;
+import org.winterblade.minecraft.harmony.utility.LogHelper;
 
 /**
  * Created by Matt on 5/17/2016.
@@ -41,6 +43,7 @@ public class AddStarterItemOperation extends BasePerPlayerOperation {
 
     @Override
     public void applyPerPlayer(EntityPlayerMP player) {
+        LogHelper.info("Adding starter set of items for player " + player.getName());
         for(ItemStack item : what) {
             player.inventory.addItemStackToInventory(item.copy());
         }
