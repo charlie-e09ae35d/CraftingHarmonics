0698f43f475b6acd20a087e8c87948e8cb599cd1
Matt <legendblade@users.noreply.github.com>
2016-05-21 12:47:27 -0700

    Abstracting NOT matchers.

--- src/main/java/org/winterblade/minecraft/harmony/blocks/drops/matchers/NotMatcher.java
+++ src/main/java/org/winterblade/minecraft/harmony/blocks/drops/matchers/NotMatcher.java
@@ -1,5 +1,6 @@
 package org.winterblade.minecraft.harmony.blocks.drops.matchers;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.world.BlockEvent;
 import org.winterblade.minecraft.harmony.api.Component;
@@ -7,33 +8,18 @@ import org.winterblade.minecraft.harmony.api.PrioritizedObject;
 import org.winterblade.minecraft.harmony.api.Priority;
 import org.winterblade.minecraft.harmony.api.blocks.IBlockDropMatcher;
 import org.winterblade.minecraft.harmony.api.BaseMatchResult;
+import org.winterblade.minecraft.harmony.api.mobs.sheds.IMobShedMatcher;
 import org.winterblade.minecraft.harmony.blocks.drops.BlockDrop;
+import org.winterblade.minecraft.harmony.drops.matchers.BaseNotMatcher;
+import org.winterblade.minecraft.harmony.mobs.sheds.MobShed;
 
 /**
  * Created by Matt on 5/13/2016.
  */
 @Component(properties = {"not"})
 @PrioritizedObject(priority = Priority.MEDIUM)
-public class NotMatcher implements IBlockDropMatcher {
-    private final BlockDrop composite;
-
+public class NotMatcher extends BaseNotMatcher<BlockEvent.HarvestDropsEvent, ItemStack, IBlockDropMatcher, BlockDrop> implements IBlockDropMatcher {
     public NotMatcher(BlockDrop composite) {
-        this.composite = composite;
-    }
-
-    /**
-     * Should return true if this matcher matches the given event
-     *
-     * @param evt The event to match
-     * @param drop             The dropped item; this can be modified.
-     * @return True if it should match; false otherwise
-     */
-    @Override
-    public BaseMatchResult isMatch(BlockEvent.HarvestDropsEvent evt, ItemStack drop) {
-        // We aren't going to run any of the callbacks from here, as they're inverted.
-        BaseMatchResult result = composite.matches(evt, drop);
-
-        // Just invert the match:
-        return new BaseMatchResult(!result.isMatch());
+        super(composite);
     }
 }
--- src/main/java/org/winterblade/minecraft/harmony/mobs/sheds/matchers/NotMatcher.java
+++ src/main/java/org/winterblade/minecraft/harmony/mobs/sheds/matchers/NotMatcher.java
@@ -1,13 +1,12 @@
 package org.winterblade.minecraft.harmony.mobs.sheds.matchers;
 
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import org.winterblade.minecraft.harmony.api.Component;
 import org.winterblade.minecraft.harmony.api.PrioritizedObject;
 import org.winterblade.minecraft.harmony.api.Priority;
-import org.winterblade.minecraft.harmony.api.BaseMatchResult;
 import org.winterblade.minecraft.harmony.api.mobs.sheds.IMobShedMatcher;
+import org.winterblade.minecraft.harmony.drops.matchers.BaseNotMatcher;
 import org.winterblade.minecraft.harmony.mobs.sheds.MobShed;
 
 /**
@@ -15,26 +14,8 @@ import org.winterblade.minecraft.harmony.mobs.sheds.MobShed;
  */
 @Component(properties = {"not"})
 @PrioritizedObject(priority = Priority.MEDIUM)
-public class NotMatcher implements IMobShedMatcher {
-    private final MobShed composite;
-
+public class NotMatcher extends BaseNotMatcher<EntityLivingBase, ItemStack, IMobShedMatcher, MobShed> implements IMobShedMatcher {
     public NotMatcher(MobShed composite) {
-        this.composite = composite;
-    }
-
-    /**
-     * Should return true if this matcher matches the given event
-     *
-     * @param entity           The event to match
-     * @param drop             The dropped item; this can be modified.
-     * @return True if it should match; false otherwise
-     */
-    @Override
-    public BaseMatchResult isMatch(EntityLivingBase entity, ItemStack drop) {
-        // We aren't going to run any of the callbacks from here, as they're inverted.
-        BaseMatchResult result = composite.matches(entity, drop);
-
-        // Just invert the match:
-        return new BaseMatchResult(!result.isMatch());
+        super(composite);
     }
 }
