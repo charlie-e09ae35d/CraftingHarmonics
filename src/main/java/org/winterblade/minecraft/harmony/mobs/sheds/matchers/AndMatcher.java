b1ff317198af33dbc31fe3099dc273a0f6438a30
Matt <legendblade@users.noreply.github.com>
2016-05-21 12:35:47 -0700

    Abstracting AND matchers.

--- src/main/java/org/winterblade/minecraft/harmony/mobs/sheds/matchers/AndMatcher.java
+++ src/main/java/org/winterblade/minecraft/harmony/mobs/sheds/matchers/AndMatcher.java
@@ -8,6 +8,7 @@ import org.winterblade.minecraft.harmony.api.PrioritizedObject;
 import org.winterblade.minecraft.harmony.api.Priority;
 import org.winterblade.minecraft.harmony.api.BaseMatchResult;
 import org.winterblade.minecraft.harmony.api.mobs.sheds.IMobShedMatcher;
+import org.winterblade.minecraft.harmony.drops.matchers.BaseAndMatcher;
 import org.winterblade.minecraft.harmony.mobs.sheds.MobShed;
 
 import java.util.ArrayList;
@@ -18,49 +19,8 @@ import java.util.List;
  */
 @Component(properties = {"and"})
 @PrioritizedObject(priority = Priority.MEDIUM)
-public class AndMatcher implements IMobShedMatcher {
-    private final MobShed[] composites;
-
+public class AndMatcher extends BaseAndMatcher<EntityLivingBase, ItemStack, IMobShedMatcher, MobShed> implements IMobShedMatcher {
     public AndMatcher(MobShed[] composites) {
-        this.composites = composites;
-    }
-
-    /**
-     * Should return true if this matcher matches the given event
-     *
-     * @param entity The event to match
-     * @param drop             The dropped item; this can be modified.
-     * @return True if it should match; false otherwise
-     */
-    @Override
-    public BaseMatchResult isMatch(EntityLivingBase entity, ItemStack drop) {
-        if(composites == null || composites.length <= 0) return BaseMatchResult.False;
-
-        List<Runnable> runnables = new ArrayList<>();
-        int callbacks = 0;
-
-        for(MobShed composite : composites) {
-            BaseMatchResult result = composite.matches(entity, drop);
-
-            // If we didn't match, we failed:
-            if(!result.isMatch()) return BaseMatchResult.False;
-
-            // Add any callbacks we need to do:
-            if(result.getCallback() != null) {
-                runnables.add(result.getCallback());
-                callbacks++;
-            }
-        }
-
-        // If we can handle this easily, do so:
-        if(callbacks <= 0) return BaseMatchResult.True;
-        if(callbacks == 1) return new BaseMatchResult(true, runnables.get(0));
-
-        // Return a composite function:
-        return new BaseMatchResult(true, () -> {
-            for(Runnable runnable : runnables) {
-                runnable.run();
-            }
-        });
+        super(composites);
     }
 }
