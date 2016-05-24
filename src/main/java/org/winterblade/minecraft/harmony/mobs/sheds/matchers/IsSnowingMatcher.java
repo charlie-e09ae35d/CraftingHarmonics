28fefb85ac1f05b75cf0a173ddfcf5c578af0f48
Matt <legendblade@users.noreply.github.com>
2016-05-23 17:38:40 -0700

    Adding isSnowing matcher.

---
+++ src/main/java/org/winterblade/minecraft/harmony/mobs/sheds/matchers/IsSnowingMatcher.java
@@ -0,0 +1,33 @@
+package org.winterblade.minecraft.harmony.mobs.sheds.matchers;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import org.winterblade.minecraft.harmony.api.BaseMatchResult;
+import org.winterblade.minecraft.harmony.api.Component;
+import org.winterblade.minecraft.harmony.api.PrioritizedObject;
+import org.winterblade.minecraft.harmony.api.Priority;
+import org.winterblade.minecraft.harmony.api.mobs.sheds.IMobShedMatcher;
+import org.winterblade.minecraft.harmony.drops.matchers.BaseIsSnowingMatcher;
+
+/**
+ * Created by Matt on 5/23/2016.
+ */
+@Component(properties = {"isSnowing"})
+@PrioritizedObject(priority = Priority.HIGH)
+public class IsSnowingMatcher extends BaseIsSnowingMatcher implements IMobShedMatcher {
+    public IsSnowingMatcher(WeatherMatcher matcher) {
+        super(matcher);
+    }
+
+    /**
+     * Should return true if this matcher matches the given event
+     *
+     * @param entityLivingBase The event to match
+     * @param drop             The dropped item; this can be modified.
+     * @return True if it should match; false otherwise
+     */
+    @Override
+    public BaseMatchResult isMatch(EntityLivingBase entityLivingBase, ItemStack drop) {
+        return matches(entityLivingBase.getEntityWorld(), entityLivingBase.getPosition());
+    }
+}
