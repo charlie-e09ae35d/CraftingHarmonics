1c248c1621830f7190bf00508d5924a23d73804c
Matt <legendblade@users.noreply.github.com>
2016-05-27 08:13:30 -0700

    Adding 'smite' callback.

---
+++ src/main/java/org/winterblade/minecraft/harmony/entities/callbacks/SmiteCallback.java
@@ -0,0 +1,24 @@
+package org.winterblade.minecraft.harmony.entities.callbacks;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.util.math.Vec3d;
+import org.winterblade.minecraft.harmony.api.entities.EntityCallback;
+import org.winterblade.minecraft.harmony.api.entities.IEntityCallbackContainer;
+
+/**
+ * Created by Matt on 5/26/2016.
+ */
+@EntityCallback(name = "smite")
+public class SmiteCallback extends VectorBaseCallback {
+    private boolean effectOnly;
+    private IEntityCallbackContainer[] onComplete;
+
+    @Override
+    protected void applyTo(Entity target) {
+        Vec3d pos = getPosition(target);
+        EntityLightningBolt bolt = new EntityLightningBolt(target.getEntityWorld(), pos.xCoord, pos.yCoord, pos.zCoord, effectOnly);
+        target.getEntityWorld().addWeatherEffect(bolt);
+        runCallbacks(onComplete, target);
+    }
+}
