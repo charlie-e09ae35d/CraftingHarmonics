a28f53799c40b7b5c405e6005a99c472f5f07fd3
Matt <legendblade@users.noreply.github.com>
2016-05-28 15:09:13 -0700

    Adding and removing crystallizer recipes.

---
+++ integration/abyssalcraft/src/main/java/org/winterblade/minecraft/harmony/integration/abyssalcraft/operations/AddCrystallizerRecipeOperation.java
@@ -0,0 +1,88 @@
+package org.winterblade.minecraft.harmony.integration.abyssalcraft.operations;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+import org.winterblade.minecraft.harmony.api.BasicOperation;
+import org.winterblade.minecraft.harmony.api.Operation;
+import org.winterblade.minecraft.harmony.api.OperationException;
+import org.winterblade.minecraft.harmony.api.crafting.RecipeInput;
+import org.winterblade.minecraft.harmony.common.ItemUtility;
+import org.winterblade.minecraft.harmony.common.utility.LogHelper;
+import org.winterblade.minecraft.harmony.integration.abyssalcraft.AbyssalCraftUtilities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Matt on 5/28/2016.
+ */
+@Operation(name = "AbyssalCraft.addCrystallizer")
+public class AddCrystallizerRecipeOperation extends BasicOperation {
+    /*
+     * Serialized properties
+     */
+    private RecipeInput with;
+    private ItemStack[] output;
+    private float xp;
+
+    /*
+     * Computed properties
+     */
+    private Map<ItemStack, ItemStack[]> recipes = new HashMap<>();
+
+    /**
+     * Called to initialize the set
+     *
+     * @throws OperationException If something went wrong
+     */
+    @Override
+    public void init() throws OperationException {
+        if(with == null) throw new OperationException("AbyssalCraft crystallization recipe must have an input ('with').");
+        if(output == null || output.length <= 0) throw new OperationException("AbyssalCraft crystallization recipe must have at least one output ('output': []).");
+        if(2 < output.length) throw new OperationException("AbyssalCraft crystallization recipes can have at most 2 outputs.");
+
+        List<ItemStack> inputs = new ArrayList<>();
+
+        // Figure out if we're an ore dictionary or not...
+        if(with.getFacsimileItem() instanceof String) {
+            inputs.addAll(OreDictionary.getOres(with.getFacsimileItem().toString()));
+        } else if(with.getFacsimileItem() instanceof ItemStack){
+            inputs.add((ItemStack) with.getFacsimileItem());
+        }
+
+        if(inputs.size() <= 0) {
+            throw new OperationException("AbyssalCraft crystallization recipe must have a valid input.");
+        }
+
+        recipes.clear();
+
+        for(ItemStack input : inputs) {
+            recipes.put(input, output);
+        }
+    }
+
+    /**
+     * Called to apply the set (if not player-specific)
+     */
+    @Override
+    public void apply() {
+        LogHelper.info("Adding AbyssalCraft crystallization producing '" + ItemUtility.outputItemName(output[0]) + "'.");
+        for(Map.Entry<ItemStack, ItemStack[]> entry : recipes.entrySet()) {
+            AbyssalCraftUtilities.crystallizationList.put(entry.getKey(), entry.getValue());
+            AbyssalCraftUtilities.crystallizerXpList.put(entry.getKey(), xp);
+        }
+    }
+
+    /**
+     * Called to remove the set (if not player-specific)
+     */
+    @Override
+    public void undo() {
+        for(Map.Entry<ItemStack, ItemStack[]> entry : recipes.entrySet()) {
+            AbyssalCraftUtilities.crystallizationList.remove(entry.getKey());
+            AbyssalCraftUtilities.crystallizerXpList.remove(entry.getKey());
+        }
+    }
+}
---
+++ integration/abyssalcraft/src/main/java/org/winterblade/minecraft/harmony/integration/abyssalcraft/operations/RemoveCrystallizerRecipeOperation.java
@@ -0,0 +1,100 @@
+package org.winterblade.minecraft.harmony.integration.abyssalcraft.operations;
+
+import com.shinoow.abyssalcraft.api.recipe.CrystallizerRecipes;
+import net.minecraft.item.ItemStack;
+import org.winterblade.minecraft.harmony.api.BasicOperation;
+import org.winterblade.minecraft.harmony.api.Operation;
+import org.winterblade.minecraft.harmony.api.OperationException;
+import org.winterblade.minecraft.harmony.common.ItemUtility;
+import org.winterblade.minecraft.harmony.common.utility.LogHelper;
+import org.winterblade.minecraft.harmony.integration.abyssalcraft.AbyssalCraftUtilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Matt on 5/28/2016.
+ */
+@Operation(name = "AbyssalCraft.removeCrystallizer")
+public class RemoveCrystallizerRecipeOperation extends BasicOperation {
+    /*
+     * Serialized properties
+     */
+    private ItemStack what;
+    private ItemStack with;
+
+    /*
+     * Computed properties
+     */
+    private Map<ItemStack, ItemStack[]> recipes = new HashMap<>();
+    private Map<ItemStack, Float> xpList = new HashMap<>();
+
+    /**
+     * Called to initialize the set
+     *
+     * @throws OperationException If something went wrong
+     */
+    @Override
+    public void init() throws OperationException {
+        if(what == null && with == null) throw new OperationException("To remove an AbyssalCraft crystallization, you must specify at least one output or input ('what' or 'with').");
+
+        recipes.clear();
+        xpList.clear();
+
+        for (Map.Entry<ItemStack, ItemStack[]> entry : AbyssalCraftUtilities.crystallizationList.entrySet()) {
+            // If we have an input, just use that...
+            if(with != null) {
+                if(!ItemUtility.areItemsEquivalent(entry.getKey(),with)) continue;
+
+                recipes.put(entry.getKey(), entry.getValue());
+                xpList.put(entry.getKey(), CrystallizerRecipes.instance().getExperience(entry.getKey()));
+                continue;
+            }
+
+            // Check if the first output matches:
+            if (ItemUtility.areItemsEquivalent(entry.getValue()[0],what)) {
+                recipes.put(entry.getKey(), entry.getValue());
+                xpList.put(entry.getKey(), CrystallizerRecipes.instance().getExperience(entry.getKey()));
+                continue;
+            }
+
+            // If we don't have a second output...
+            if (entry.getValue()[1] == null) continue;
+
+            // Check if the second output matches
+            if (ItemUtility.areItemsEquivalent(entry.getValue()[1],what)) {
+                recipes.put(entry.getKey(), entry.getValue());
+                xpList.put(entry.getKey(), CrystallizerRecipes.instance().getExperience(entry.getKey()));
+            }
+        }
+    }
+
+    /**
+     * Called to apply the set (if not player-specific)
+     */
+    @Override
+    public void apply() {
+        if(with != null) {
+            LogHelper.info("Removing AbyssalCraft crystallization using '" + ItemUtility.outputItemName(with) + "'.");
+        } else {
+            LogHelper.info("Removing AbyssalCraft crystallizations producing '" + ItemUtility.outputItemName(what) + "'.");
+        }
+
+        for(Map.Entry<ItemStack, ItemStack[]> entry : recipes.entrySet()) {
+            AbyssalCraftUtilities.crystallizationList.remove(entry.getKey());
+            AbyssalCraftUtilities.crystallizerXpList.remove(entry.getKey());
+        }
+    }
+
+    /**
+     * Called to remove the set (if not player-specific)
+     */
+    @Override
+    public void undo() {
+        for(Map.Entry<ItemStack, ItemStack[]> entry : recipes.entrySet()) {
+            AbyssalCraftUtilities.crystallizationList.put(entry.getKey(), entry.getValue());
+            AbyssalCraftUtilities.crystallizerXpList.put(entry.getKey(), xpList.get(entry.getKey()));
+        }
+    }
+}
+
