203028561bdfe5b835e0877012fba49e31c68b95
Matt <legendblade@users.noreply.github.com>
2016-05-28 20:13:26 -0700

    Fixing an issue where the default duration of a potion was 1 tick, but should've been 0.

--- src/main/java/org/winterblade/minecraft/harmony/entities/callbacks/ApplyPotionCallback.java
+++ src/main/java/org/winterblade/minecraft/harmony/entities/callbacks/ApplyPotionCallback.java
@@ -32,7 +32,7 @@ public class ApplyPotionCallback extends BaseEntityCallback {
     private Potion what;
     private int amplifier;
     private boolean showParticles;
-    private int duration = 1;
+    private int duration;
     private ItemStack[] cures;
     private IEntityCallbackContainer[] onApplied;
     private IEntityCallbackContainer[] onNew;
