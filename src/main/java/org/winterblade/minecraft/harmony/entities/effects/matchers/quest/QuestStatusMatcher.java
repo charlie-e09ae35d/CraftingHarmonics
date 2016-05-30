dd2e66f66073d9a1e791514664ddb5b91a01acca
Matt <legendblade@users.noreply.github.com>
2016-05-30 11:50:50 -0700

    Adding entity quest matchers.

---
+++ src/main/java/org/winterblade/minecraft/harmony/entities/effects/matchers/quest/QuestStatusMatcher.java
@@ -0,0 +1,26 @@
+package org.winterblade.minecraft.harmony.entities.effects.matchers.quest;
+
+import net.minecraft.entity.Entity;
+import org.winterblade.minecraft.harmony.api.BaseMatchResult;
+import org.winterblade.minecraft.harmony.api.Component;
+import org.winterblade.minecraft.harmony.api.PrioritizedObject;
+import org.winterblade.minecraft.harmony.api.Priority;
+import org.winterblade.minecraft.harmony.api.entities.IEntityMatcherData;
+import org.winterblade.minecraft.harmony.api.mobs.effects.IEntityMatcher;
+import org.winterblade.minecraft.harmony.quests.matchers.BaseQuestStatusMatcher;
+
+/**
+ * Created by Matt on 5/30/2016.
+ */
+@Component(properties = {"questingQuestStatus"})
+@PrioritizedObject(priority = Priority.LOWER)
+public class QuestStatusMatcher extends BaseQuestStatusMatcher implements IEntityMatcher {
+    public QuestStatusMatcher(QuestStatusMatchData data) {
+        super(data);
+    }
+
+    @Override
+    public BaseMatchResult isMatch(Entity entity, IEntityMatcherData iEntityMatcherData) {
+        return matches(entity);
+    }
+}
