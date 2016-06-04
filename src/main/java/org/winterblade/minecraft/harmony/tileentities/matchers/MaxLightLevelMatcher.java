a68e6459b503f8547f1b4ccd11f86feec597c302
Matt <legendblade@users.noreply.github.com>
2016-06-04 13:58:41 -0700

    Light level matchers.

---
+++ src/main/java/org/winterblade/minecraft/harmony/tileentities/matchers/MaxLightLevelMatcher.java
@@ -0,0 +1,26 @@
+package org.winterblade.minecraft.harmony.tileentities.matchers;
+
+import net.minecraft.tileentity.TileEntity;
+import org.winterblade.minecraft.harmony.api.BaseMatchResult;
+import org.winterblade.minecraft.harmony.api.Component;
+import org.winterblade.minecraft.harmony.api.PrioritizedObject;
+import org.winterblade.minecraft.harmony.api.Priority;
+import org.winterblade.minecraft.harmony.api.tileentities.ITileEntityMatcher;
+import org.winterblade.minecraft.harmony.api.tileentities.ITileEntityMatcherData;
+import org.winterblade.minecraft.harmony.common.matchers.BaseLightLevelMatcher;
+
+/**
+ * Created by Matt on 6/4/2016.
+ */
+@Component(properties = {"maxLightLevel"})
+@PrioritizedObject(priority = Priority.HIGH)
+public class MaxLightLevelMatcher extends BaseLightLevelMatcher implements ITileEntityMatcher {
+    public MaxLightLevelMatcher(int max) {
+        super(0,max);
+    }
+
+    @Override
+    public BaseMatchResult isMatch(TileEntity tileEntity, ITileEntityMatcherData iTileEntityMatcherData) {
+        return matches(tileEntity.getWorld(), tileEntity.getPos());
+    }
+}
