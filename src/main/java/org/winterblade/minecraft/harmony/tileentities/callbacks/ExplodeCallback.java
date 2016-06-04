90ec11a9763d3dbc9df1a0acf9e6a2810865c108
Matt <legendblade@users.noreply.github.com>
2016-06-04 16:14:09 -0700

    Adding in burn and explode callbacks.

---
+++ src/main/java/org/winterblade/minecraft/harmony/tileentities/callbacks/ExplodeCallback.java
@@ -0,0 +1,37 @@
+package org.winterblade.minecraft.harmony.tileentities.callbacks;
+
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import org.winterblade.minecraft.harmony.api.tileentities.ITileEntityCallback;
+import org.winterblade.minecraft.harmony.api.tileentities.TileEntityCallback;
+
+/**
+ * Created by Matt on 6/4/2016.
+ */
+@TileEntityCallback(name = "explode")
+public class ExplodeCallback extends CoordinateBaseCallback {
+    /*
+     * Serialized properties
+     */
+    private float strength;
+    private boolean isSmoking;
+    private ITileEntityCallback[] onComplete;
+
+    /**
+     * Allows the instance to do any last minute updating it needs to, if necessary
+     *
+     * @param mirror The mirror to update from
+     */
+    @Override
+    protected void finishDeserialization(ScriptObjectMirror mirror) throws RuntimeException {
+        if(strength < 0) throw new RuntimeException("Explosion strength cannot be less than zero.");
+    }
+
+    @Override
+    protected void applyTo(TileEntity target, Data data) {
+        BlockPos pos = getPosition(target);
+        target.getWorld().createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), strength, isSmoking);
+        runCallbacks(onComplete, target);
+    }
+}
