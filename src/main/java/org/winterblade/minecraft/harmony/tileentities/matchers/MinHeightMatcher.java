4eab5a9bf65073374662082cd833fe527a4a513e
Matt <legendblade@users.noreply.github.com>
2016-06-04 14:00:48 -0700

    Height matchers.

---
+++ src/main/java/org/winterblade/minecraft/harmony/tileentities/matchers/MinHeightMatcher.java
@@ -0,0 +1,24 @@
+package org.winterblade.minecraft.harmony.tileentities.matchers;
+
+import net.minecraft.tileentity.TileEntity;
+import org.winterblade.minecraft.harmony.api.BaseMatchResult;
+import org.winterblade.minecraft.harmony.api.Component;
+import org.winterblade.minecraft.harmony.api.PrioritizedObject;
+import org.winterblade.minecraft.harmony.api.Priority;
+import org.winterblade.minecraft.harmony.api.tileentities.ITileEntityMatcher;
+import org.winterblade.minecraft.harmony.api.tileentities.ITileEntityMatcherData;
+import org.winterblade.minecraft.harmony.common.matchers.BaseHeightMatcher;
+
+/**
+ * Created by Matt on 6/4/2016.
+ */
+@Component(properties = {"minHeight"})
+@PrioritizedObject(priority = Priority.HIGHER)
+public class MinHeightMatcher extends BaseHeightMatcher implements ITileEntityMatcher {
+    public MinHeightMatcher(int level) {super(level, Integer.MAX_VALUE);}
+
+    @Override
+    public BaseMatchResult isMatch(TileEntity tileEntity, ITileEntityMatcherData iTileEntityMatcherData) {
+        return matches(tileEntity.getPos());
+    }
+}
