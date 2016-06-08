82a42901e26b9721a8e4571f37e284d4b3288daf
Matt <legendblade@users.noreply.github.com>
2016-06-07 18:34:09 -0700

    Adding Entity Area target modifiers.

--- api/src/main/java/org/winterblade/minecraft/harmony/api/entities/IEntityTargetModifier.java
+++ api/src/main/java/org/winterblade/minecraft/harmony/api/entities/IEntityTargetModifier.java
@@ -2,11 +2,11 @@ package org.winterblade.minecraft.harmony.api.entities;
 
 import net.minecraft.entity.Entity;
 
-import java.util.Set;
+import java.util.List;
 
 /**
  * Created by Matt on 5/24/2016.
  */
 public interface IEntityTargetModifier {
-    Set<Entity> getTargets(Entity source);
+    List<Entity> getTargets(Entity source);
 }
---
+++ src/main/java/org/winterblade/minecraft/harmony/entities/targets/AreaTargetModifier.java
@@ -0,0 +1,26 @@
+package org.winterblade.minecraft.harmony.entities.targets;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.AxisAlignedBB;
+import org.winterblade.minecraft.harmony.api.Component;
+import org.winterblade.minecraft.harmony.api.entities.IEntityTargetModifier;
+
+import java.util.List;
+
+/**
+ * Created by Matt on 6/7/2016.
+ */
+@Component(properties = {"area"})
+public class AreaTargetModifier implements IEntityTargetModifier {
+    private final double radius;
+
+    public AreaTargetModifier(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public List<Entity> getTargets(Entity source) {
+        AxisAlignedBB aabb = new AxisAlignedBB(source.getPosition()).expandXyz(radius);
+        return source.getEntityWorld().getEntitiesWithinAABB(Entity.class, aabb);
+    }
+}
