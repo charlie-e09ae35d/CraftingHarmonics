ef1dc3e9ab689c5db8f40ef459330e00217b7032
Matt <legendblade@users.noreply.github.com>
2016-06-07 19:54:30 -0700

    Adding an "except" parameter to multitarget selectors.

--- src/main/java/org/winterblade/minecraft/harmony/entities/targets/MultitargetTargetModifier.java
+++ src/main/java/org/winterblade/minecraft/harmony/entities/targets/MultitargetTargetModifier.java
@@ -11,12 +11,18 @@ import java.util.List;
 /**
  * Created by Matt on 6/7/2016.
  */
-@Component(properties = {"multitarget"})
+@Component(properties = {"multitarget", "except"})
 public class MultitargetTargetModifier implements IEntityTargetModifier {
     private final IEntityTargetModifier[] modifiers;
+    private final IEntityTargetModifier[] except;
 
     public MultitargetTargetModifier(IEntityTargetModifier[] modifiers) {
+        this(modifiers, new IEntityTargetModifier[0]);
+    }
+
+    public MultitargetTargetModifier(IEntityTargetModifier[] modifiers, IEntityTargetModifier[] except) {
         this.modifiers = modifiers;
+        this.except = except;
     }
 
     @Override
@@ -26,6 +32,10 @@ public class MultitargetTargetModifier implements IEntityTargetModifier {
 
         for(IEntityTargetModifier modifier : modifiers) {
             output.addAll(modifier.getTargets(source, data));
+        }
+
+        for(IEntityTargetModifier modifier : except) {
+            output.removeAll(modifier.getTargets(source, data));
         }
 
         return output;
