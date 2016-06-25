88eb8a399b941571871fa8f89b6ad157e4082d21
Matt <legendblade@users.noreply.github.com>
2016-06-24 18:50:50 -0700

    Adding itemstack checking, still need to add in operations to modify them.

--- src/main/java/org/winterblade/minecraft/harmony/items/ItemRegistry.java
+++ src/main/java/org/winterblade/minecraft/harmony/items/ItemRegistry.java
@@ -1,6 +1,15 @@
 package org.winterblade.minecraft.harmony.items;
 
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import org.winterblade.minecraft.harmony.api.crafting.RecipeInput;
+import org.winterblade.minecraft.harmony.common.ItemUtility;
+
+import javax.annotation.Nonnull;
+import java.util.Set;
 
 /**
  * Created by Matt on 6/22/2016.
@@ -10,12 +19,34 @@ public class ItemRegistry {
 
     public static final ItemRegistry instance = new ItemRegistry();
 
+    private final Multiset<RecipeInput> preventedItems = HashMultiset.create();
+
     /**
      * Called to determine if an item should be allowed to be used
      * @param evt    The event to check
      * @return       True if it should be cancelled, false otherwise.
      */
     public boolean shouldCancelUse(PlayerInteractEvent evt) {
+        for (RecipeInput preventedItem : preventedItems) {
+            if(ItemUtility.recipeInputsMatch(preventedItem.getFacsimileItem(), evt.getItemStack())) return true;
+        }
         return false;
+    }
+
+
+    /**
+     * Adds the given stacks to the list of items to prevent using.
+     * @param stacks    The stacks to prevent.
+     */
+    public void preventUse(@Nonnull Set<RecipeInput> stacks) {
+        preventedItems.addAll(stacks);
+    }
+
+    /**
+     * Removes the given stacks from the list of items to prevent using
+     * @param stacks    The stacks to allow again.
+     */
+    public void allowUse(@Nonnull Set<RecipeInput> stacks) {
+        preventedItems.removeAll(stacks);
     }
 }
