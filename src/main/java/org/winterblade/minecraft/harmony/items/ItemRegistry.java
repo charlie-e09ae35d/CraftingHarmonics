4fc937418faa49922d05aaa902cec6e44e6ffc8c
Matt <legendblade@users.noreply.github.com>
2016-06-24 20:42:34 -0700

    Adding matchers and callbacks to item prevention.

--- src/main/java/org/winterblade/minecraft/harmony/items/ItemRegistry.java
+++ src/main/java/org/winterblade/minecraft/harmony/items/ItemRegistry.java
@@ -1,12 +1,17 @@
 package org.winterblade.minecraft.harmony.items;
 
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import org.winterblade.minecraft.harmony.api.BaseMatchResult;
 import org.winterblade.minecraft.harmony.api.crafting.RecipeInput;
+import org.winterblade.minecraft.harmony.api.entities.IEntityCallback;
+import org.winterblade.minecraft.harmony.api.mobs.effects.IEntityMatcher;
+import org.winterblade.minecraft.harmony.common.BaseEntityMatcherData;
 import org.winterblade.minecraft.harmony.common.ItemUtility;
+import org.winterblade.minecraft.harmony.mobs.MobTickRegistry;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Matt on 6/22/2016.
@@ -16,7 +21,7 @@ public class ItemRegistry {
 
     public static final ItemRegistry instance = new ItemRegistry();
 
-    private final Multiset<RecipeInput> preventedItems = HashMultiset.create();
+    private final List<ItemBlockData> preventedItems = new ArrayList<>();
 
     /**
      * Called to determine if an item should be allowed to be used
@@ -24,8 +29,8 @@ public class ItemRegistry {
      * @return       True if it should be cancelled, false otherwise.
      */
     public boolean shouldCancelUse(PlayerInteractEvent evt) {
-        for (RecipeInput preventedItem : preventedItems) {
-            if(ItemUtility.recipeInputsMatch(preventedItem.getFacsimileItem(), evt.getItemStack())) return true;
+        for (ItemBlockData preventedItem : preventedItems) {
+            if(preventedItem.matches(evt)) return true;
         }
         return false;
     }
@@ -34,16 +39,64 @@ public class ItemRegistry {
     /**
      * Adds the given input to the list of items to prevent using.
      * @param input    The input to prevent.
+     * @param onBlock  The callbacks to run when it's been blocked
+     * @param matchers The matchers to check to do the blocking.
      */
-    public void preventUse(@Nonnull RecipeInput input) {
-        preventedItems.add(input);
+    public ItemBlockData preventUse(@Nonnull RecipeInput[] input, IEntityCallback[] onBlock, List<IEntityMatcher> matchers) {
+        ItemBlockData data = new ItemBlockData(input, onBlock, matchers);
+        preventedItems.add(data);
+        return data;
     }
 
     /**
      * Removes the given input from the list of items to prevent using
      * @param input    The input to allow again.
      */
-    public void allowUse(@Nonnull RecipeInput input) {
+    public void allowUse(@Nonnull ItemBlockData input) {
         preventedItems.remove(input);
+    }
+
+    public static class ItemBlockData {
+        private final RecipeInput[] inputs;
+        private final IEntityCallback[] onBlock;
+        private final List<IEntityMatcher> matchers;
+
+        private ItemBlockData(RecipeInput[] inputs, IEntityCallback[] onBlock, List<IEntityMatcher> matchers) {
+            this.inputs = inputs;
+            this.onBlock = onBlock;
+            this.matchers = matchers;
+        }
+
+        private boolean matches(PlayerInteractEvent evt) {
+            // Quickly check our inputs...
+            boolean matches = false;
+            for (RecipeInput input : inputs) {
+                if(!ItemUtility.recipeInputsMatch(input.getFacsimileItem(), evt.getItemStack())) continue;
+                matches = true;
+                break;
+            }
+
+            if(!matches) return false;
+
+            List<Runnable> runnables = new ArrayList<>();
+            BaseEntityMatcherData matcherData = new BaseEntityMatcherData(evt.getEntity());
+
+            // Now check our matchers...
+            for(IEntityMatcher matcher : matchers) {
+                BaseMatchResult res = matcher.isMatch(evt.getEntity(), matcherData);
+                if(!res.isMatch()) return false;
+                if(res.getCallback() != null) runnables.add(res.getCallback());
+            }
+
+            // Fire the runnables from our matchers...
+            for (Runnable runnable : runnables) {
+                runnable.run();
+            }
+
+            // Also fire our callbacks...
+            MobTickRegistry.addCallbackSet(evt.getEntity(), onBlock, matcherData);
+
+            return true;
+        }
     }
 }
