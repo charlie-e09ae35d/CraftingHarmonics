9af703aaab0a93d3690001344bf6a12efba3c295
Matt <legendblade@users.noreply.github.com>
2016-07-06 16:05:04 -0700

    Allowing runPlayerCommand to work on all living entities.

--- src/main/java/org/winterblade/minecraft/harmony/entities/callbacks/CommandCallback.java
+++ src/main/java/org/winterblade/minecraft/harmony/entities/callbacks/CommandCallback.java
@@ -27,14 +27,14 @@ public class CommandCallback extends BaseEntityCallback {
     @Override
     public void applyTo(Entity target, CallbackMetadata data) {
         if(target.getEntityWorld().isRemote) return;
-        if(!EntityPlayerMP.class.isAssignableFrom(target.getClass())) {
-            LogHelper.info("Not running player command '{}' on target as target is not a player ({})", command, target.getClass().getName());
+        if(!EntityLivingBase.class.isAssignableFrom(target.getClass())) {
+            LogHelper.info("Not running command '{}' on target as target is not a living entity ({})", command, target.getClass().getName());
             return;
         }
 
         if(name != null) name = name.replaceAll("@p", target.getName());
 
-        CommandCallbackSender sender = new CommandCallbackSender((EntityPlayerMP) target, name);
+        CommandCallbackSender sender = new CommandCallbackSender((EntityLivingBase) target, name);
 
         for (String s : command) {
             String specificCommand = s.replaceAll("@p", target.getName());
